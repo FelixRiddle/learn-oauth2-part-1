ff42571e45bcb3c574c09d8e575f61059cfdcb7d
Felix <fedecraft3312@gmail.com>
2024-10-28 21:14:14 -0300

    Save authorization token and get authorization token

--- src/OAuth2.ts
+++ src/OAuth2.ts
@@ -1,4 +1,5 @@
 import { Model, Mongoose } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
 
 /**
  * OAuth2
@@ -22,23 +23,60 @@ export default class OAuth2 {
 	 * Get client
 	 */
 	async getClient(clientId: string, clientSecret: string) {
-		const client = await this.OAuthClients.findOne({
+		const client: any = await this.OAuthClients.findOne({
 			clientId,
 			...(clientSecret && { clientSecret }),
 		}).lean();
-		
-		if(!client) {
+
+		if (!client) {
 			throw new Error("Client not found");
 		}
-		
+
 		return {
 			id: client.clientId,
 			grants: client.grants,
-			redirectUris: [client.callbackUrl]
+			redirectUris: [client.callbackUrl],
 		};
 	}
-	
+
 	/**
-	 * 
+	 * Save authorization code
 	 */
+	async saveAuthorizationCode(code: any, client: any, user: any) {
+		const authorizationCode = {
+			authorizationCode: code.authorizationCode,
+			expiresAt: code.expiresAt,
+			redirectUri: code.redirectUri,
+			scope: code.scope,
+			clientId: client.id,
+			userId: user._id,
+		};
+		await this.OAuthAuthorizationCodes.create({ _id: uuidv4() });
+		return authorizationCode;
+	}
+
+	/**
+	 * Get authorization code
+	 */
+	async getAuthorizationCode(authorizationCode: string) {
+		const code: any = await this.OAuthAuthorizationCodes.findOne({
+			authorizationCode,
+		}).lean();
+		if(!code) {
+			throw new Error("Authorization code not found");
+		}
+		
+		return {
+			code: code.authorizationCode,
+			expiresAt: code.expiresAt,
+			redirectUri: code.redirectUri,
+			scope: code.scope,
+			client: {
+				id: code.clientId
+			},
+			user: {
+				id: code.userId,
+			}
+		};
+	}
 }
